aa9eb4f614d46e4ca1d08942d03e154a338c0db4
Sergio Herrera <herrera.se1992@gmail.com>
2021-04-07 12:18:41 -0400

    First Commit , creacion de archivo sql y analisis

---
+++ caso1.sql
@@ -0,0 +1,7 @@
+--Caso 1
+
+----Un trabajador pertenece a un departamento.
+----El trabajador tiene un nombre, rut y dirección.
+----El departamento tiene un nombre.
+----Un trabajador posee liquidaciones.
+----La liquidación se guarda en un archivo drive.
